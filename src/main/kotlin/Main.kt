d7ea5927e5b472a96af1007b7801a65fe1dabf19
Ahmad Ali <111639866+muhasabah@users.noreply.github.com>
2022-08-21 23:06:40 +0800

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main(args: Array<String>) {
 
     val namaKamu = "Binarian"
-    // namaKamu = "Faldi"
+    // namaKamu = "Cewek"
     // Ini merupakan tipe variabel immutable yang artinya data didalamnya tidak bisa dimodifikasi
 
-    var namaSaya = "Faldi"
-    namaSaya = "Faldiansyah"
+    var namaSaya = "Ahmad"
+    namaSaya = "Ahmad Ali"
     // Ini merupakan tipe variabel mutable yang artinya data didalamnya bisa dimodifikasi
 
     println(namaKamu)
